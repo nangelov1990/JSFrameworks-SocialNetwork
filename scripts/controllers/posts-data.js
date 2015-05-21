658d7fba42e2bcbe09bdec7c80387f3715f30566
Nick Angelov <nangelov1990@gmail.com>
2015-05-21 22:38:10 +0300

    Setting up controller for posts

---
+++ scripts/controllers/posts-data.js
@@ -0,0 +1,53 @@
+"use strict";
+
+SocialNetwork.factory('postsData', function ($resource) {
+    var resourse = $resource(
+        baseServiceUrl + 'Posts/:id',
+        { id: '@id'},
+        { update: {
+                method: 'PUT'
+            }
+        });
+
+
+
+    function createNewPost(post) {
+        return resourse.save(post);
+    };
+
+    function getPostById(id) {
+        return resourse.get({id: id});
+    };
+
+    function editPost(id, postContent) {
+        return resourse.update({id: id}, postContent);
+    };
+
+    function deletePost(id) {
+        return resourse.delete({id: id});
+    };
+
+    function getNumberOfPostLikes(id) {
+        var idForLikes = id + '/likes';
+
+        return resourse.get({id: idForLikes});
+    };
+
+    function getPostLikesUsers(id) {
+        var idForLikes = id + '/likes/preview';
+
+        return resourse.get({id: idForLikes});
+    };
+
+    function likePost(id) {
+        var idOfPost = id + '/likes';
+        
+        return resourse.getAllResponseHeaders({id: idOfPost});        
+    };
+
+    function unlikePost(id) {
+        var idOfPost = id + '/likes';
+
+        return resourse.get({id: idOfPost});
+    };
+});
