9301414368872a33469c9d622e00c8c74a23da0e
Nick Angelov <nangelov1990@gmail.com>
2015-05-22 23:57:04 +0300

    Setting up services and partials

---
+++ scripts/services/authentication.js
@@ -0,0 +1,12 @@
+"use strict";
+
+SocialNetwork.factory('authenticaiton', function ($http, baseServiceUrl) {
+    var authentiocation ={},
+        authentiocationUrl = baseServiceUrl + '/users/';
+
+    authentiocation.params = {};
+
+    authentiocation.register = function () {
+        
+    }
+});
